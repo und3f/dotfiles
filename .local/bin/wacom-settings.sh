7a00c923e4bfb17b766760adeff2995762908e59
Sergey Zasenko <sergii@zasenko.name>
2016-08-28 13:55:28 +0300

    set wacom settings

---
+++ .local/bin/wacom-settings.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh
+
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 10 key "shift"
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 11 key "ctrl" 
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 12 "key Alt_L"
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 13 "key z"
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 2 key p        
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 3 key n    
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 8 key "shift e"
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 9 key s    
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" AbsWheelDown key ']' 
+xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" AbsWheelUp key '['   
