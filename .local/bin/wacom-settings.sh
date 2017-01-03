e70407932202051e9cb22e63faa9c300e587b0b1
Sergey Zasenko <sergii@zasenko.name>
2017-01-03 11:30:15 +0200

    fixed wacom settings

--- .local/bin/wacom-settings.sh
+++ .local/bin/wacom-settings.sh
@@ -1,13 +1,14 @@
 
 #!/bin/sh
 
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 10 key "shift"
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 11 key "ctrl" 
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 12 "key Alt_L"
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 13 "key z"
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 2 key p        
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 3 key n    
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 8 key "shift e"
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" Button 9 key s    
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" AbsWheelDown key ']' 
-xsetwacom set "Wacom Intuos Pro M (WL) Pen pad" AbsWheelUp key '['   
+device=$(xsetwacom --list devices | grep PAD | awk -F'\\s*id' '{print $1}')
+xsetwacom set "$device" Button 10 key "shift"
+xsetwacom set "$device" Button 11 key "ctrl" 
+xsetwacom set "$device" Button 12 "key Alt_L"
+xsetwacom set "$device" Button 13 "key z"
+xsetwacom set "$device" Button 2 key p        
+xsetwacom set "$device" Button 3 key n    
+xsetwacom set "$device" Button 8 key "shift e"
+xsetwacom set "$device" Button 9 key s    
+xsetwacom set "$device" AbsWheelDown key ']' 
+xsetwacom set "$device" AbsWheelUp key '['   
